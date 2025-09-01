e896645655cc754d07580d7f80c95c3b2f8c7539
matheusballardini <159540170+matheusballardini@users.noreply.github.com>
2025-09-01 07:28:14 -0300

    Add files via upload

---
+++ CODIGO CHURRASCARIA/src/com/java/sga/model/Itens.java
@@ -0,0 +1,43 @@
+package com.java.sga.model;
+
+
+
+public abstract class Itens {
+    private int id;
+    private String nome;
+    private String tipo;
+
+    //Construtor
+
+    public Itens(int id, String nome, String tipo) {
+        this.id = id;
+        this.nome = nome;
+        this.tipo = tipo;
+    }
+    //Getters e Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+}
